2714529f969bea445728400f87281e86b7f26b7b
mahaitao <ma@qiandaodao.com>
2018-05-02 15:17:09 +0800

    mdoify

--- BlueTooth/Login/LoginViewController.m
+++ BlueTooth/Login/LoginViewController.m
@@ -8,6 +8,7 @@
 
 #import "LoginViewController.h"
 #import "Toast.h"
+#import "BlueTooth-Swift.h"
 
 @interface LoginViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *headImage;
@@ -17,6 +18,8 @@
 @property (strong, nonatomic) IBOutlet UITextField *sleepField;
 
 @property (strong, nonatomic) NSMutableDictionary *userDic;
+
+@property (strong, nonatomic) Toast *toast;
 
 @end
 
@@ -35,34 +38,38 @@
 }
 - (IBAction)loginBtnAction:(UIButton *)sender {
     
-//    if (self.nameField.text.length == 0) {
-//        //请填写姓名
-//        return;
-//    }
-//    
-//    if (self.weightField.text.length == 0) {
-//        //请填写
-//         return;
-//    }
-//    
-//    if (self.drinkField.text.length == 0) {
-//        //请填写
-//         return;
-//    }
-//    
-//    if (self.sleepField.text.length == 0) {
-//        //请填写
-//         return;
-//    }
+    if (self.nameField.text.length == 0) {
+        //请填写姓名
+        [DSToast show:@"请填写姓名"];
+        return;
+    }
     
-//    [self.userDic setObject:self.nameField.text forKey:@"name"];
-//    [self.userDic setObject:self.weightField.text forKey:@"weight"];
-//    [self.userDic setObject:self.drinkField.text forKey:@"drink"];
-//    [self.userDic setObject:self.sleepField.text forKey:@"sleep"];
-//    [self.userDic setObject:self.headImage.image forKey:@"headImage"];
+    if (self.weightField.text.length == 0) {
+        //请填写
+        [DSToast show:@"请填写体重目标"];
+        return;
+    }
+    
+    if (self.drinkField.text.length == 0) {
+        //请填写
+        [DSToast show:@"请填写喝水目标"];
+        return;
+    }
+    
+    if (self.sleepField.text.length == 0) {
+        //请填写
+        [DSToast show:@"请填写睡眠目标"];
+         return;
+    }
+    
+    [self.userDic setObject:self.nameField.text forKey:@"name"];
+    [self.userDic setObject:self.weightField.text forKey:@"weight"];
+    [self.userDic setObject:self.drinkField.text forKey:@"drink"];
+    [self.userDic setObject:self.sleepField.text forKey:@"sleep"];
+    [self.userDic setObject:self.headImage.image forKey:@"headImage"];
     
     [[NSUserDefaults standardUserDefaults]setObject:@"1" forKey:@"isLogin"];
-//    [[NSUserDefaults standardUserDefaults]setObject:self.userDic forKey:@"userDic"];
+    [[NSUserDefaults standardUserDefaults]setObject:self.userDic forKey:@"userDic"];
     
     [self dismissViewControllerAnimated:NO completion:nil];
     
