fea0ab713f609c052b6e1b7ae47993c29588f96f
mahaitao <ma@qiandaodao.com>
2018-05-06 23:34:48 +0800

    启动图

--- BlueTooth/Menu/Controller/SideViewController.m
+++ BlueTooth/Menu/Controller/SideViewController.m
@@ -74,7 +74,7 @@
     
     self.sourceArray = [NSMutableArray array];
     NSArray* strArray = @[@"我的数据",@"储物冰箱",@"旅行日志",@"旅行商店",@"荣誉徽章",@"同行好友"];
-    NSArray* imageArray = @[@"我的数据icon",@"储物冰箱icon",@"旅行日志icon",@"商店",@"徽章",@"我的好友"];
+    NSArray* imageArray = @[@"我的数据icon",@"储物冰箱",@"旅行日志",@"商店",@"徽章",@"我的好友"];
     for (int i = 0; i < 6; i++) {
         SlideModel *model = [[SlideModel alloc]init];
         model.titleStr= strArray[i];
@@ -130,14 +130,13 @@
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
     UIView *headView = [[UIView alloc]init];
-//    headView.backgroundColor = [UIColor redColor];
     return headView;
 }
 
 
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 20;
+    return 21;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
--- BlueTooth/Menu/View/SlideCell.m
+++ BlueTooth/Menu/View/SlideCell.m
@@ -51,7 +51,7 @@
     [self.titleImage mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(ws.contentView.mas_centerY);
         make.left.equalTo(ws.contentView.mas_left).with.offset(90);
-        make.size.mas_equalTo(CGSizeMake(20 , 20));
+        make.size.mas_equalTo(CGSizeMake(22 , 21));
     }];
     
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
